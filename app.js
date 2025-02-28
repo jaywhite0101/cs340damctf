b13d6e761f81c4ee1e6aa521df683ac817e3db3c
Alexa Baruela <baruelaa@oregonstate.edu>
2025-02-27 16:15:34 -0800

    changed port number

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2539;
 
 // Create the Handlebars instance with helpers
 const hbs = exphbs.create({
